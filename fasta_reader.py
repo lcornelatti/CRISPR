e28be96b5903f6c0aefd32b3904fd1fbdca195a7
leocorne <leonardocornelatti@gmail.com>
2018-11-14 15:27:38 +0000

    Custom PAM and fixed code

--- fasta_reader.py
+++ fasta_reader.py
@@ -1,11 +1,10 @@
 # import sys
 
 
-def get_string(filename):
+def read_fasta(filename):
     fasta = {}
     active_sequence_name = ''
-    seq_name = 'ref|NW_018734404.1| Aedes aegypti strain LVP_AGWG chromosome 1 genomic scaffold, AaegL5.0 Primary ' + \
-               'Assembly, whole genome shotgun sequence'
+    # seq_name = '22 dna:chromosome chromosome:GRCh38:22:1:50818468:1 REF'
     with open(filename) as file_one:
 
         for line in file_one:
@@ -21,13 +20,47 @@ def get_string(filename):
             fasta[active_sequence_name].append(sequence)
 
     for key in fasta.keys():
-        print(key)
-        print(fasta[key][0])
+        return fasta[key]
 
 
+def remove_ns(sequence_list):
+    start = 0
+    end = 0
+    for i in range(0, len(sequence_list)):
+        if not (sequence_list[i].startswith("NNN") and sequence_list[i].endswith("NNN")):
+            start = i
+            break
 
-    return fasta[seq_name]
+    for i in range(start, len(sequence_list)):
+        if sequence_list[i].startswith("NNN") and sequence_list[i].endswith("NNN"):
+            end = i
+            break
+
+    assert end != 0 and start < end
+
+    return sequence_list[start:end]
 
 
-genome = get_string("C:\\Users\\leoco\\Desktop\\Project\\hs_ref_GRCh38.p12_chr22.fa")
-print(len(genome))
+def remove_ns2(sequence_list):
+    return [x for x in sequence_list if list(set(x)) != ['N']]
+
+
+def list_to_string(list):
+    nstring = ''
+    for item in list:
+        nstring += item
+
+    return nstring
+
+
+def file_to_string(filepath):
+    fasta_sequence = read_fasta(filepath)
+    fasta_sequence2 = remove_ns2(fasta_sequence)
+    string_genome = ''.join(fasta_sequence2)
+    print(len(string_genome))
+    return string_genome
+
+
+if __name__ == "__main__":
+    fp = r"C:\Users\leoco\Desktop\Project\Homo_sapiens.GRCh38.dna.chromosome.22.fa"
+    file_to_string(fp)
--- search.py
+++ search.py
@@ -1,24 +1,52 @@
-M = [0, 0, 0.014, 0, 0, 0.395, 0.317, 0, 0.389, 0.079, 0.445, 0.508, 0.613, 0.851, 0.732, 0.828, 0.615, 0.804, 0.685, 0.583]
+from pprint import pprint
+import fasta_reader
+import time
 
+
+M = [0, 0, 0.014, 0, 0, 0.395, 0.317, 0, 0.389, 0.079, 0.445, 0.508, 0.613, 0.851, 0.732, 0.828, 0.615, 0.804, 0.685, 0.583]
+max_mismatches = 5
+
+# Validate IUPAC code to see if 2 strings are the same
+# Strcheck MUST be composed of ACGT only, otherwise this will not work
+def validate_iupac(strref, strcheck):
+
+    assert len(strref) == len(strcheck)
+
+    for cref, ccheck in zip(strref, strcheck):
+        if ccheck == 'A':
+            if cref not in ('AWMRDHVN'): return False
+        elif ccheck == 'C':
+            if cref not in ('CSMYBHVN'): return False
+        elif ccheck == 'G':
+            if cref not in ('GSKRBDVN'): return False
+        elif ccheck == 'T':
+            if cref not in ('TWKYBDHN'): return False
+        else: return False
+    return True
+
+# Gets complementary DNA strand
 def get_complement(sequence):
     lower = sequence.lower()
     comp = lower.replace('t', 'A').replace('a', 'T').replace('g', 'C').replace('c', 'G')
     return comp
 
 
-def find_guides(sequence):
+# Finds potential guides within a sequence (must contain an appropriate PAM)
+def find_guides(sequence, guide_length, pam_ref):
     guides = []
 
-    for i in range(21, len(sequence)-1):
-        j = len(sequence) - i
-        if sequence[i] == 'G' and sequence[i+1] == 'G':
-            guides.append([sequence[(i-21):(i-1)],sequence[(i-1):(i+2)]])
-        if sequence[j] == 'G' and sequence[j-1] == 'G':
-            guides.append([sequence[(j+2):(j+22)][::-1],sequence[(j-1):(j+2)][::-1]])
+    for i in range(0, (len(sequence) - (len(pam_ref) + guide_length) + 1)):
+
+        i_guide = sequence[i:i + guide_length]
+        i_pam = sequence[i+guide_length:i+guide_length+len(pam_ref)]
+
+        if validate_iupac(pam_ref, i_pam):
+            guides.append({'sequence':i_guide, 'pam':i_pam})
 
     return guides
 
 
+# Calculates position of differences between two strings
 def calculate_differences(str1, str2):
     assert len(str1) == len(str2)
     diffs = []
@@ -28,20 +56,33 @@ def calculate_differences(str1, str2):
     return diffs
 
 
-# Get potential offtargets. Only works if PAM is exactly equal
-def get_potential_offtargets(guide, genome):
+# Get potential offtargets
+def get_potential_offtargets(guide, genome, max_mismatches, pam_ref):
+
     potential_offtargets = []
-    for i in range(0, len(genome)-22):
-        this_guide = genome[(i+20):(i+23)]
-        this_sequence = genome[i:i+20]
-        if genome[(i+20):(i+23)] == guide[1]:
-            diffs = calculate_differences(this_sequence, guide[0])
-            if len(diffs) < 5:
-                potential_offtargets.append([this_sequence, diffs, i])
+    guide_length = len(guide['sequence'])
+    pam_length = len(pam_ref)
+
+    for i in range(0, (len(genome) - pam_length - guide_length + 1)):
+
+        this_sequence = genome[i:i + guide_length]
+        this_pam = genome[(i+guide_length):(i+guide_length+pam_length)]
+
+        if validate_iupac(pam_ref, this_pam):
+            diffs = calculate_differences(this_sequence, guide['sequence'])
+
+            if len(diffs) <= max_mismatches:
+                potential_offtargets.append({'sequence': this_sequence,
+                                             'differences': diffs,
+                                             'start': i,
+                                             'pam': this_pam
+                                             })
+
     return potential_offtargets
 
 
-def sumPairs(arr):
+# Return the sum of all pairs in an array
+def sum_pairs(arr):
     n = len(arr)
     sum = 0
     for i in range(n - 1, -1, -1):
@@ -49,8 +90,9 @@ def sumPairs(arr):
     return sum
 
 
+# Return single hit score of an offtarget hit
 def single_hit_score(offtarget):
-    print('gf is cute')
+
     num_mis = len(offtarget)
 
     # Calculate weight of each mismatch
@@ -59,40 +101,57 @@ def single_hit_score(offtarget):
         t1 *= (1-M[mismatch])
 
     # Calculate penalty for mismatch distance
-    dsum = sumPairs(offtarget)
+    dsum = sum_pairs(offtarget)
     pairs = num_mis*(num_mis-1)*0.5
     d = dsum/pairs if pairs != 0 else 0
     t2 = 1/(((19-d)/19)*4 + 1)
 
     # Calculate penalty for high mismatch
     t3 = 1/(num_mis**2)
-    return t1*t2*t3
+    return 100*t1*t2*t3
+
 
 def main():
-
-    genome = ('attcagaTCCTAATCCAGAATCCAACAGGGaattatataggaaaaACCACTTCCAGAATCCAACAGGGatatttcatctagactggtccgtccccagcacccaactgactccccgggatccgaggctccctgagttg' +
-             'caatgccgtggtaccgaaatcaACCGGATCCAGAATCCAACAGGGgtagagtgtccgccgtgttgtccgcgactagacgt').upper()
+    start = time.time()
+    # Get genome from file and calculate complement and reverse
+    genome_fp = r"C:\Users\leoco\Desktop\Project\Homo_sapiens.GRCh38.dna.chromosome.22.fa"
+    genome = fasta_reader.file_to_string(genome_fp)
+    # genome = 'gctcattacgacccgagaccgacgcagacgtggtaataccaacgaactcggatgttctaaaggtgtttgcttcctacgcgtcggtgcaatcctggtagtg'.upper()
     genome_comp = get_complement(genome)
     genome_reverse = genome[::-1]
     genome_comp_reverse = genome_comp[::-1]
-    sequence = 'atccacctaatccagaatccaacagggctt'.upper()
-    complement = get_complement(sequence)
-    guides = find_guides(sequence)
-    comp_guides = find_guides(complement)
-    guides = guides + comp_guides
-    scores = []
 
+    # Get all possible guides
+    sequence = 'atccacctaatccagaatccaacagggctt'.upper()
+    pam_ref = 'NGG'
+    guide_length = 20
+    complement = get_complement(sequence)
+    guides = find_guides(sequence, guide_length, pam_ref)
+    comp_guides = find_guides(complement, guide_length, pam_ref)
+    rev_guides = find_guides(sequence[::-1], guide_length, pam_ref)
+    crev_guides = find_guides(complement[::-1], guide_length, pam_ref)
+    guides = guides + comp_guides + rev_guides + crev_guides
+    # pprint(guides)
+
+    # For each guide calculate score
     for guide in guides:
-        offtargets = get_potential_offtargets(guide, genome)
-        offtargets += get_potential_offtargets(guide, genome_reverse)
-        offtargets += get_potential_offtargets(guide, genome_comp)
-        offtargets += get_potential_offtargets(guide, genome_comp_reverse)
-        print('xd')
-        print(offtargets)
+
+        offtargets = get_potential_offtargets(guide, genome, max_mismatches, pam_ref)
+        offtargets += get_potential_offtargets(guide, genome_reverse, max_mismatches, pam_ref)
+        offtargets += get_potential_offtargets(guide, genome_comp, max_mismatches, pam_ref)
+        offtargets += get_potential_offtargets(guide, genome_comp_reverse, max_mismatches, pam_ref)
+
+        scores_sum = 0
         for item in offtargets:
-            print(guide, item)
-            shs = single_hit_score(item[1])
-            print(shs)
+            shs = single_hit_score(item['differences'])
+            item['shs'] = shs
+            scores_sum += shs
+        guide['score'] = 100 * (100/(100 + scores_sum))
+        guide['potential_offtargets'] = offtargets
+
+    pprint(guides)
+    end = time.time()
+    print(end-start)
 if __name__ == "__main__":
     main()
 
